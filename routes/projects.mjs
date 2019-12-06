f17baee882c7b440588c0c414d563f2d39b65809
DeAndre Johnson <dre@phreshr.com>
2019-12-07 00:50:27 +0800

    add GET project by ID route

--- routes/projects.mjs
+++ routes/projects.mjs
@@ -3,9 +3,23 @@ import projectModel from "../data/helpers/projectModel.js";
 
 const router = express.Router();
 
+// Get All
 router.get("/", async (req, res) => {
   try {
     const projects = await projectModel.get();
+    res.send(projects);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get by ID
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const projects = await projectModel.get(id);
+    if (!projects)
+      return res.status(404).send({ message: "No project with that ID found" });
     res.send(projects);
   } catch (error) {
     res.status(500).send(error);
