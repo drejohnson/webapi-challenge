d6433ddc8b34b42f984189ea587b2cec0ef6dd9d
DeAndre Johnson <dre@phreshr.com>
2019-12-07 01:05:08 +0800

    use POST to add projects

--- routes/projects.mjs
+++ routes/projects.mjs
@@ -20,7 +20,22 @@ router.get("/:id", async (req, res) => {
     const projects = await projectModel.get(id);
     if (!projects)
       return res.status(404).send({ message: "No project with that ID found" });
-    res.send(projects);
+    res.status(200).send(projects);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Add project
+router.post("/", async (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description)
+    return res.status(400).json({
+      errorMessage: "Please provide name and description for the post."
+    });
+  try {
+    const project = await projectModel.insert(req.body);
+    res.status(201).send(project);
   } catch (error) {
     res.status(500).send(error);
   }
